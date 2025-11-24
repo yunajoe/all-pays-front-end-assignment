720f5c97449ea610a4283a9740b4ac4079a241d3
yunajoe <yunajoe@gmail.com>
2025-11-24 16:29:43 +0900

    chore: global css 셋팅 및 폴더 구조 정리

---
+++ app/merchants/[id]/page.tsx
@@ -0,0 +1,5 @@
+function MerchantsDetailPage() {
+  return <div>가맹점 상세 페이지 입니다.</div>;
+}
+
+export default MerchantsDetailPage;
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+function NotFoundPage() {
+  return <div>요청하신 페이지를 찾을 수 없습니다.</div>;
+}
+
+export default NotFoundPage;
