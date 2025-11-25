f99a26ae3d2e87beb8d9e0299b7ef0c6255e3a27
yunajoe <yunajoe@gmail.com>
2025-11-25 17:53:48 +0900

    feat: 거래내역 조회 API 연결

---
+++ app/api/payments/index.ts
@@ -0,0 +1,6 @@
+// 거래 내역 전체 조회
+// https://recruit.paysbypays.com/api/v1/payments/list
+
+import { BASE_URL } from "..";
+
+export const getPaymentsListUrl = `${BASE_URL}/payments/list`;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,30 @@
-function DashBoardPage() {
-  return <div>DashBoardPage입니당 </div>;
+import { getPaymentsListUrl } from "../api/payments";
+import KpiContainer from "./components/kpi/kpiContainer";
+import styles from "./dashboard.module.css";
+
+async function getPayments() {
+  const response = await fetch(getPaymentsListUrl);
+  if (!response.ok) {
+    throw new Error("[Error] 가맹점 상세 정보를 불러올 수 없습니다.");
+  }
+  return response.json();
+}
+
+async function DashBoardPage() {
+  const payments = await getPayments();
+  const data = payments.data;
+  return (
+    <div className={styles.container}>
+      {/* 1. KPIs / 핵심 지표  */}
+      <KpiContainer />
+
+      {/* 2. Top 5 가맹점   */}
+      <div className={styles.merchantContainer}></div>
+
+      {/* 결제 수단별  */}
+      <div className={styles.paymentMethodContainer}></div>
+    </div>
+  );
 }
 
 export default DashBoardPage;
