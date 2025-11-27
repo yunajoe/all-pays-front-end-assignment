b9c85866b9977b17af2cb534a5420afa135c4dd6
yunajoe <yunajoe@gmail.com>
2025-11-27 23:18:53 +0900

    style: 결제내역 리스트 페이지 UI

--- app/payments/components/payment-list.tsx
+++ app/payments/components/payment-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Payment } from "@/app/types/payment";
 import { useState } from "react";
+import { convertDateFromISO } from "../utils";
 import styles from "./payment-list.module.css";
 interface PaymentListProps {
   data: Payment[];
@@ -14,13 +15,21 @@ function PaymentList({ data }: PaymentListProps) {
         <h2 className={styles.title}>거래 내역 리스트 목록</h2>
         <div className={styles.cardListContainer}>
           {paymentsData.map((item, index) => {
-            const { paymentCode, mchtCode, amount, payType, status } = item;
+            const {
+              paymentCode,
+              mchtCode,
+              amount,
+              payType,
+              status,
+              paymentAt,
+            } = item;
             return (
               <div key={index} className={styles.card}>
-                <h3>{mchtCode}</h3>
-                <p>{amount}</p>
+                <h3>결제 코드: {paymentCode}</h3>
+                <p>결제 금액: {amount}</p>
                 <p>결제 수단 : {payType}</p>
-                <p>{status}</p>
+                <p>결제 상태: {status}</p>
+                <p>결제 날짜: {convertDateFromISO(paymentAt)}</p>
               </div>
             );
           })}
