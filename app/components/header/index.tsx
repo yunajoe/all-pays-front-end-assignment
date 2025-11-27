6426acfd2abd231c9196c031538dc701075dbcf8
yunajoe <yunajoe@gmail.com>
2025-11-27 22:51:26 +0900

    feat: 거래내역 리스트

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -6,8 +6,8 @@ function Header() {
       <p>올페이즈 결제 시스템</p>
       <ul className={styles.menuList}>
         <Link href="/">대시보드</Link>
+        <Link href="/payments">거래내역 관리</Link>
         <Link href="/merchants">가맹점 관리</Link>
-        <Link href="/payments">결제 관리</Link>
       </ul>
     </nav>
   );
--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,5 +1,20 @@
-function PaymentsPage() {
-  return <div>결제 관련 페이지 </div>;
+import { getPaymentsListUrl } from "../api/payments";
+import PaymentList from "./components/payment-list";
+
+async function getPayments() {
+  const response = await fetch(getPaymentsListUrl, {
+    next: { revalidate: 10 },
+  });
+  if (!response.ok) {
+    throw new Error("[Error] 거래 내역 정보를 불러올 수 없습니다.");
+  }
+  return response.json();
+}
+
+async function PaymentsPage() {
+  const payments = await getPayments();
+  const data = payments.data;
+  return <PaymentList data={data} />;
 }
 
 export default PaymentsPage;
